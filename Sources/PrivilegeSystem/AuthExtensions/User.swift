2a148bc8af6a067752a90e46089ebae23406f0e5
ChenLin Wang <wangchenlin2001@icloud.com>
2026-06-24 13:07:26 +0800

    V1.0.4.2 修改项目中错误的 PrivilegeModuleExtended 名称依赖

--- Sources/PrivilegeSystem/AuthExtensions/User.swift
+++ Sources/PrivilegeSystem/AuthExtensions/User.swift
@@ -1,5 +1,5 @@
 import Vapor
-import PrivilegeModels
+import PrivilegeModuleExtended
 
 // MARK: - ModelAuthenticatable
 
--- Sources/PrivilegeSystem/Exports.swift
+++ Sources/PrivilegeSystem/Exports.swift
@@ -1,1 +1,1 @@
-@_exported import PrivilegeModels
+@_exported import PrivilegeModuleExtended
